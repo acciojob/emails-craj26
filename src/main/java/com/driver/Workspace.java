60cb3468e9e935eefcfb3f0573fc900e27bbe560
Arpit <arpitjain7254@gmail.com>
2023-03-06 13:49:53 +0530

    copy update

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -5,6 +5,7 @@ import org.apache.commons.lang3.tuple.Pair;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.*;
 
 public class Workspace extends Gmail{
 
@@ -20,35 +21,77 @@ public class Workspace extends Gmail{
         //add the meeting to calendar
         calendar.add(meeting);
     }
+    public class CustomComparator implements Comparator<Meeting> {
+        @Override
+        public int compare(Meeting t1, Meeting t2) {
+            int val = t1.getEndTime().compareTo(t2.getEndTime());
+            if(val == 0){
+                return (t1.getStartTime().compareTo(t2.getStartTime()))>0?1:-1;
+            }else{
+                return (t1.getEndTime().compareTo(t2.getEndTime()))>0?1:-1;
+            }
+//            if(t1.getEndTime().compareTo(t2.getEndTime())>0){
+//                return 1;
+//            }else if(t1.getEndTime().compareTo(t2.getEndTime())<0){
+//                return -1;
+//            }else if(t1.getEndTime().compareTo(t2.getEndTime())==0){
+//                if(t1.getStartTime().compareTo(t2.getStartTime())>0){
+//                    return 1;
+//                }else if(t1.getStartTime().compareTo(t2.getStartTime())<0){
+//                    return -1;
+//                }
+//            }
+//            return 0;
+        }
+    }
 
     public int findMaxMeetings(){
         // find the maximum number of meetings you can attend
-        // at a particular time, you can only be present in at most one meeting
-        // if you want to attend a meeting, you must join it at its start time and leave at end time.
+        // 1. At a particular time, you can be present in at most one meeting
+        // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
+        // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
+        int count = 1;
+        calendar.sort(new CustomComparator());
 
-        ArrayList<Pair<LocalTime, Integer>> endTimes = new ArrayList<>();
+        LocalTime prevEnd = calendar.get(0).getEndTime();
 
-        for (int i = 0; i < calendar.size(); i++) {
-            endTimes.add(Pair.of(calendar.get(i).getEndTime(), i));
-        }
+        for (int i = 1; i < calendar.size(); i++)
+        {
+            //LocalTime prevEnd = calendar.get(i-1).getEndTime();
 
-        Collections.sort(endTimes);
+            LocalTime currStart = calendar.get(i).getStartTime();
+            if(prevEnd.compareTo(currStart) < 0){
+                count++;
+                prevEnd = calendar.get(i).getEndTime();
 
-        LocalTime time_limit = endTimes.get(0).getLeft();
-
-        int cnt = 0;
-        if(!endTimes.isEmpty()) {
-            cnt += 1;
-        }
-
-        for (int i = 1; i < endTimes.size(); i++) {
-            if (calendar.get(endTimes.get(i).getRight()).getStartTime().compareTo(time_limit) > 0) {
-
-                cnt += 1;
-                time_limit = endTimes.get(i).getLeft();
             }
+
         }
 
-        return cnt;
+        return count;
+//        ArrayList<Pair<LocalTime, Integer>> endTimes = new ArrayList<>();
+//
+//        for (int i = 0; i < calendar.size(); i++) {
+//            endTimes.add(Pair.of(calendar.get(i).getEndTime(), i));
+//        }
+//
+//        Collections.sort(endTimes);
+//
+//        LocalTime time_limit = endTimes.get(0).getLeft();
+//
+//        int cnt = 0;
+//        if(!endTimes.isEmpty()) {
+//            cnt += 1;
+//        }
+//
+//        for (int i = 1; i < endTimes.size(); i++) {
+//            if (calendar.get(endTimes.get(i).getRight()).getStartTime().compareTo(time_limit) > 0) {
+//
+//                cnt += 1;
+//                time_limit = endTimes.get(i).getLeft();
+//            }
+//        }
+//
+//        return cnt;
     }
 }
